ff3ebd6d7bf531ab9436bedbedc62d6a3643c977
Oliver Ni <oliver.ni@gmail.com>
2021-05-23 03:05:21 -0700

    Move into apps directory

--- apps/lichen/mix.exs
+++ apps/lichen/mix.exs
@@ -5,6 +5,10 @@ defmodule Lichen.MixProject do
     [
       app: :lichen,
       version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
