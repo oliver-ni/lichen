410ef5642dbc3c1efc3f32f288147c7b68c04a36
Oliver Ni <oliver.ni@gmail.com>
2021-05-24 13:03:45 -0700

    Reimplement preprocessing w/ backtracing

--- apps/lichen/lib/lichen/language.ex
+++ apps/lichen/lib/lichen/language.ex
@@ -11,6 +11,19 @@ defmodule Lichen.Language do
     end
   end
 
+  @doc """
+  Returns a list of regex patterns matching comments in the language.
+  """
+  @spec comment_regex(Lichen.Language.Config.t()) :: [Regex.t()]
+  def comment_regex(%Lichen.Language.Config{comments: %{open: open, close: close, line: line}}) do
+    (Enum.zip(
+       Enum.map(open, &Regex.escape(&1)),
+       Enum.map(close, &Regex.escape/1)
+     )
+     |> Enum.map(fn {open, close} -> ~r/#{open}.+?#{close}/s end)) ++
+      Enum.map(line, &~r/#{Regex.escape(&1)}.+$/m)
+  end
+
   @typedoc """
   A programming language with its own preprocessing options.
   """
--- apps/lichen/lib/lichen/preprocessing.ex
+++ apps/lichen/lib/lichen/preprocessing.ex
@@ -9,58 +9,79 @@ defmodule Lichen.Preprocessing do
 
   @doc """
   Preprocesses the string by removing comments, splitting on whitespace
-  and special characters into a series of tokens, and renaming all
-  generic (non-keyword) identifiers.
+  and special characters, and renaming all generic (non-keyword) identifiers,
+  into a stream of tuples in the form `{token, start_index}`.
 
   ## Examples
 
       iex> str = "List<Character> myList = List.of('A', 'B', 'C');"
-      ...> Lichen.Preprocessor.preprocess(str, Lichen.Language.Java)
-      "List<Character>i=List.i('i','i','i');"
-
+      ...> Lichen.Preprocessing.preprocess(str, Lichen.Language.Java)
+      [
+        {"List", 0},
+        {"<", 4},
+        {"Character", 5},
+        {">", 14},
+        {"i", 16},
+        {"=", 23},
+        {"List", 25},
+        {".", 29},
+        {"i", 30},
+        {"(", 32},
+        {"'", 33},
+        {"i", 34},
+        {"'", 35},
+        {",", 36},
+        {"'", 38},
+        {"i", 39},
+        {"'", 40},
+        {",", 41},
+        {"'", 43},
+        {"i", 44},
+        {"'", 45},
+        {")", 46},
+        {";", 47}
+      ]
   """
-  @spec preprocess(String.t(), Lichen.Language.t()) :: String.t()
+  @spec preprocess(String.t(), Lichen.Language.t()) :: Enumerable.t({String.t(), integer()})
   def preprocess(str, language) when is_atom(language) do
     config = language.config()
 
     str
     |> remove_comments(config)
     |> to_tokens(config)
-    |> Enum.flat_map(&String.split/1)
-    |> Enum.map(&String.trim/1)
+    |> with_indexes()
+    |> Stream.map(fn {x, i} -> {String.trim(x), i} end)
+    |> Stream.reject(fn {x, _} -> x == "" end)
     |> rename_identifiers(config)
-    |> Enum.join()
+    |> Enum.to_list()
   end
 
-  defp remove_comments(str, %Language.Config{comments: %{open: open, close: close, line: line}}) do
-    Enum.zip(
-      Enum.map(open, &Regex.escape(&1)),
-      Enum.map(close, &Regex.escape/1)
+  defp remove_comments(str, %Language.Config{} = config) do
+    config
+    |> Language.comment_regex()
+    |> Enum.reduce(
+      str,
+      &Regex.replace(&1, &2, fn x -> String.duplicate(" ", String.length(x)) end)
     )
-    |> Enum.map(fn {open, close} -> ~r/#{open}.+?#{close}/s end)
-    |> Enum.concat(Enum.map(line, &~r/#{Regex.escape(&1)}.+$/m))
-    |> Enum.reduce(str, &Regex.replace(&1, &2, ""))
   end
 
-  defp to_tokens(str, config, acc \\ "")
-  defp to_tokens("", %Language.Config{} = _config, ""), do: []
-  defp to_tokens("", %Language.Config{} = _config, acc), do: [acc]
+  defp to_tokens(str, %Language.Config{} = config) do
+    pattern =
+      [~S"\s" | config.special_chars |> Enum.map(&Regex.escape/1)]
+      |> Enum.join("|")
 
-  defp to_tokens(str, %Language.Config{} = config, acc) do
-    {char, next} = String.next_grapheme(str)
+    Regex.split(~r/(#{pattern})/, str, include_captures: true)
+  end
 
-    if char in config.special_chars do
-      [acc, char | to_tokens(next, config)]
-    else
-      to_tokens(next, config, acc <> char)
-    end
+  defp with_indexes(tokens) do
+    Stream.transform(tokens, 0, &{[{&1, &2}], &2 + String.length(&1)})
   end
 
   defp rename_identifiers(tokens, %Language.Config{keywords: keywords, special_chars: sp_chars}) do
-    Enum.map(tokens, fn token ->
+    Stream.map(tokens, fn {token, i} ->
       if Integer.parse(token) != :error or token in keywords or token in sp_chars,
-        do: token,
-        else: @identifier
+        do: {token, i},
+        else: {@identifier, i}
     end)
   end
 end
